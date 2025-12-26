0f8a4803870597d4d6157f2f10b311556c4005a7
Jakub Bujak <Bujak-J1@ulster.ac.uk>
2025-12-26 20:38:29 +0000

    Github connecting 9

--- backend/api/app/db.py
+++ backend/api/app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./edushare.db"
+DATABASE_URL = "sqlite:////home/edushare.db"  # ✅ absolute path in /home
 
 engine = create_engine(
     DATABASE_URL,
