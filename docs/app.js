4b8bd155f0b2810592ad8fd6bc2c1f22ea98e9c0
Jakub Bujak <Bujak-J1@ulster.ac.uk>
2025-12-26 19:46:26 +0000

    Github connecting 8

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,5 @@
 (function () {
-  const API_BASE = "http://127.0.0.1:8000";
+  const API_BASE = "https://edushare-h9d4ffeqh2fqacfz.germanywestcentral-01.azurewebsites.net";
 
   // ---------------- LOGIN PAGE ----------------
   const loginForm = document.getElementById("loginForm");
